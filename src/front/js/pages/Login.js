8169880380ff6c08b494677784367593c542f646
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2024-01-22 07:09:51 +0000

    refactor(login): make minor adjustments to style

--- src/front/js/pages/Login.js
+++ src/front/js/pages/Login.js
@@ -10,18 +10,18 @@ const Login = () => {
   return (
     <div className='d-flex flex-column-reverse flex-md-row min-vh-100'>
       {/* Form Panel */}
-      <div className="d-flex flex-column justify-content-center align-items-center w-100 w-md-50 form-panel">
+      <div className="d-flex flex-column justify-content-center align-items-center w-100 w-md-50 form-panel p-4">
         {/* Form Container */}
         <div className="d-flex flex-column w-100" style={{maxWidth: "470px"}}>
 
           {/* Logo */}
-          <figure className='logo-container mb-5'>
+          <figure className='logo-container mb-4 mb-md-5'>
             <img src={logo} alt="Logo" />
           </figure>
 
           {/* Text */}
-          <h1 className='fw-bold lh-1 mb-3'>Bienvenido <br />nuevamente!</h1>
-          <p className='fs-7 mb-5'>¿No tienes un usuario? <span className="ps-1 fw-medium" onClick={() => navigate("/register")}>Regístrate</span></p>
+          <h1 className='fw-bold lh-1 mb-2 mb-md-3'>Bienvenido <br />nuevamente!</h1>
+          <p className='fs-7 mb-4 mb-md-5'>¿No tienes un usuario? <span className="ps-1 fw-medium" onClick={() => navigate("/register")}>Regístrate</span></p>
 
           {/* Form */}
           <form className='d-flex flex-column w-100'>
@@ -32,16 +32,20 @@ const Login = () => {
             </div>
 
             {/* Password */}
-            <div className="mb-5">
-              <label htmlFor="password" className="form-label">Password</label>
-              <input type="password" className="form-control mb-2" id="password" placeholder='Ingresa tu password' />
+            <div className="mb-4 mb-md-5">
+              <label htmlFor="password" className="form-label">Contraseña</label>
+              <input type="password" className="form-control mb-2" id="password" placeholder='Ingresa tu contraseña' />
               <p className='text-end fs-7 fw-medium'>¿Olvidaste tu contraseña?</p>
             </div>
 
             {/* Buttons */}
-            <div className="d-flex justify-content-end row gap-2">
-              <button type='button' className='btn btn-outline-primary col-3 rounded-4'>Cancelar</button>
-              <button type='button' className="btn btn-primary col-4 rounded-4">Login</button>
+            <div className="row g-0 justify-content-end">
+              <div className="col-4 col-md-3 me-2">
+                <button type='button' className='btn btn-outline-primary rounded-4 w-100'>Cancelar</button>
+              </div>
+              <div className="col-5">
+                <button type='button' className="btn btn-primary rounded-4 w-100">Login</button>
+              </div>
             </div>
           </form>
         </div>
