6f6955fc72232d717d3493d6303c9b04965bb3a8
Ilaria Barbierato <145076192+IlariaBa@users.noreply.github.com>
2024-01-23 21:30:01 +0000

    navbar 2nd commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,56 +5,50 @@ import logo from "../../img/el_refugio_logo.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg">
-			<div className="container">
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex align-items-center">
-						<li className="nav-item me-4">
-							<Link to="/" className="logo-container">
-								<img src={logo} alt="Logo" />
-							</Link>
-						</li>
-						<li className="nav-item mx-3">
-							<Link to="/" className="text-body-color fw-medium text-decoration-none">
+		<nav class="navbar navbar-expand-lg">
+			<div class="container">
+				<Link to="/" className="logo-container me-4">
+					<img src={logo} alt="Logo" />
+				</Link>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarText">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<Link to="/" className="mt-3 mt-lg-0 nav-link text-body-color fw-medium text-decoration-none">
 								Inicio
 							</Link>
 						</li>
-						<li className="nav-item mx-3">
-							<Link to="/animal-list" className="text-body-color fw-medium text-decoration-none">
+						<li class="nav-item">
+							<Link to="/animal-list" className="nav-link text-body-color fw-medium text-decoration-none">
 								Peluditos
 							</Link>
 						</li>
-						<li className="nav-item mx-3">
-							<Link to="/recomendations" className="text-body-color fw-medium text-decoration-none">
+						<li class="nav-item">
+							<Link to="/recomendations" className="nav-link text-body-color fw-medium text-decoration-none">
 								Información
 							</Link>
 						</li>
-						<li className="nav-item mx-3">
-							<Link to="/about" className="text-body-color fw-medium text-decoration-none">
+						<li class="nav-item">
+							<Link to="/about" className="nav-link text-body-color fw-medium text-decoration-none">
 								Sobre Nosotros
 							</Link>
 						</li>
-						<li className="nav-item mx-3">
-							<Link to="/about" className="text-body-color fw-medium text-decoration-none">
+						<li class="nav-item">
+							<Link to="/about" className="nav-link text-body-color fw-medium text-decoration-none">
 								Contacto
 							</Link>
 						</li>
 					</ul>
-					<ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-center">
-						<li className="nav-item mx-3">
-							<Link to="/register" className="text-body-color fw-medium text-decoration-none">
-								Regístrate
-							</Link>
-						</li>
-						<li className="nav-item ms-3">
-							<Link to="/login" className="text-body-color fw-medium text-decoration-none">
-								Ingresa
-							</Link>
-						</li>
-					</ul>
+					<Link to="/register" className="me-3 mb-3 mb-lg-0 nav-link ms-auto text-body-color fw-medium text-decoration-none">
+						Regístrate
+					</Link>
+					<Link to="/login" className="nav-link text-body-color fw-medium text-decoration-none">
+						Ingresa
+					</Link>
 				</div>
 			</div>
 		</nav>
-
 	);
 };
