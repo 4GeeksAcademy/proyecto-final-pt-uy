2d025915072e2735e92a40f41ccb2a097f48d5df
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2024-01-27 23:33:18 +0000

    feat(migrations/env.py): import alembic_postgresql_enum

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,8 @@ from flask import current_app
 
 from alembic import context
 
+import alembic_postgresql_enum
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
