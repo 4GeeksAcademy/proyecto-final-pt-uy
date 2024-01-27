4ba5f6b07f74398c45654b312a16fcb2e791d191
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2024-01-27 18:27:39 +0000

    refactor(navBar): change links when user is logged in

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useUserContext } from "../contexts/userContext";
 
 import logo from "../../img/el_refugio_logo.png";
 
+
 export const Navbar = () => {
+	const navigate = useNavigate();
+	const {store, actions} = useUserContext();
+
+	const handleLogout = () => {
+		actions.setToken("");
+		actions.setUser({
+			id: "", 
+			name: "", 
+			role: ""
+		});
+		navigate("/login");
+	}
+
 	return (
-		<nav className="navbar navbar-expand-lg">
+		<nav className="navbar navbar-expand-lg my-3">
 			<div className="container">
 				<Link to="/" className="logo-container me-4">
 					<img src={logo} alt="Logo" />
 				</Link>
+
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
+
 				<div className="collapse navbar-collapse" id="navbarText">
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
@@ -41,12 +59,26 @@ export const Navbar = () => {
 							</Link>
 						</li>
 					</ul>
-					<Link to="/register" className="me-3 mb-3 mb-lg-0 nav-link ms-auto text-body-color fw-medium text-decoration-none">
-						Regístrate
+					<Link 
+						className="me-3 mb-3 mb-lg-0 nav-link ms-auto text-body-color fw-medium text-decoration-none"
+						to={store.user.id ? "/profile" : "/register"} 
+					>
+						{store.user.id ? "Perfil" : "Regístrate"}
 					</Link>
-					<Link to="/login" className="nav-link text-body-color fw-medium text-decoration-none">
-						Ingresa
-					</Link>
+
+					{ 
+						!store.user.id && 
+						<Link to="/login" className="nav-link text-body-color fw-medium text-decoration-none">
+							Ingresa
+						</Link>
+					}
+					{ 
+						store.user.id && 
+						<button className="nav-link text-body-color fw-medium text-decoration-none" onClick={handleLogout}>
+							Logout
+						</button> 
+					}
+					
 				</div>
 			</div>
 		</nav>
