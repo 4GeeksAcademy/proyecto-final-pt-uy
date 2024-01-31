4318cb59f3b2fd503f50dc0fcb4d8883103b501d
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2024-01-31 00:06:54 +0000

    feat(animal-list filter panel): synchronize checkboxes with the AnimalContext store

--- src/front/js/component/animalListLeftPanel.js
+++ src/front/js/component/animalListLeftPanel.js
@@ -1,6 +1,12 @@
 import React from "react";
 
+import { useAnimalsContext } from '../contexts/animalsContext';
+
+
 export default function AnimalListLeftPanel() {
+    const { store: {filters, sorting, pagination}, actions: {setFilters} } = useAnimalsContext();
+
+
     return (
         <div className='d-flex flex-column p-4 p-md-2 ps-md-0 me-2' style={{ width: "250px" }}>
             {/* Title */}
@@ -13,14 +19,14 @@ export default function AnimalListLeftPanel() {
                 <h4 className='fw-semibold fs-6 mb-2'>Especie</h4>
 
                 <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="dog"  />
+                <input className="form-check-input" type="checkbox" value="" id="dog" checked={filters.types.dog} />
                 <label className="form-check-label" htmlFor="dog">
                     Perro
                 </label>
                 </div>
 
                 <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="cat"  />
+                <input className="form-check-input" type="checkbox" value="" id="cat" checked={filters.types.cat} />
                 <label className="form-check-label" htmlFor="cat">
                     Gato
                 </label>
@@ -32,14 +38,14 @@ export default function AnimalListLeftPanel() {
                 <h4 className='fw-semibold fs-6 mb-2'>Sexo</h4>
 
                 <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="male"  />
+                <input className="form-check-input" type="checkbox" value="" id="male" checked={filters.genders.male} />
                 <label className="form-check-label" htmlFor="male">
                     Macho
                 </label>
                 </div>
 
                 <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="female"  />
+                <input className="form-check-input" type="checkbox" value="" id="female" checked={filters.genders.female} />
                 <label className="form-check-label" htmlFor="female">
                     Hembra
                 </label>
@@ -51,21 +57,21 @@ export default function AnimalListLeftPanel() {
                 <h4 className='fw-semibold fs-6 mb-2'>Tamaño</h4>
 
                 <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="small"  />
+                <input className="form-check-input" type="checkbox" value="" id="small" checked={filters.sizes.small} />
                 <label className="form-check-label" htmlFor="small">
                     Pequeño
                 </label>
                 </div>
 
                 <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="medium"  />
+                <input className="form-check-input" type="checkbox" value="" id="medium" checked={filters.sizes.medium} />
                 <label className="form-check-label" htmlFor="medium">
                     Mediano
                 </label>
                 </div>
 
                 <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="large"  />
+                <input className="form-check-input" type="checkbox" value="" id="large" checked={filters.sizes.large} />
                 <label className="form-check-label" htmlFor="large">
                     Grande
                 </label>
