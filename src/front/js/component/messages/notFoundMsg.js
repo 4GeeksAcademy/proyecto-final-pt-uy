9e2b9857f8f019ec69436ea8dcde14067b6b8c55
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2024-02-04 01:21:41 +0000

    refactor(NotFoundMsg component): add message parameter

--- src/front/js/component/messages/notFoundMsg.js
+++ src/front/js/component/messages/notFoundMsg.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function NotFoundMsg() {
+export default function NotFoundMsg({message="No encontramos peluditos"}) {
     return (
-        <div className='d-flex flex-column w-100 align-items-center'>
+        <div className='d-flex flex-column w-100 align-items-center my-4'>
             <figure className='d-flex justify-content-center overflow-hidden w-100 mb-4' style={{ maxWidth: "200px" }}>
                 <img className='w-100' src="https://res.cloudinary.com/dnwfyqslx/image/upload/v1706800999/Site/notFound_a0yxua.png" />
             </figure>
-            <p className='fw-semibold text-center'>No encontramos peluditos <br />según los filtros activos.</p>
+            <p className='fw-semibold text-center'>{message}</p>
         </div>
     );
 }
