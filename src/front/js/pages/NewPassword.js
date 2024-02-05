b7dd221a54d301b8c0e572a484224d67cdc995ad
Roman Monfort <137888802+romanmonfort@users.noreply.github.com>
2024-02-05 21:24:48 +0000

    image error

--- src/front/js/pages/NewPassword.js
+++ src/front/js/pages/NewPassword.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-import newPassword from "../../img/newPassword.jpg";
-import logo from "../../img/el_refugio_logo.png";
+
 
 
 const NewPassword = () => {
