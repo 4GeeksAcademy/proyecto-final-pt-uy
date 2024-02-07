ae89ff9c8f7bace7309afb310d96d583071ca795
Ilaria Barbierato <145076192+IlariaBa@users.noreply.github.com>
2024-02-07 14:35:36 +0000

    Link logo login

--- src/front/js/pages/Login.js
+++ src/front/js/pages/Login.js
@@ -53,9 +53,9 @@ const Login = () => {
         <div className="d-flex flex-column w-100" style={{ maxWidth: "470px" }}>
 
           {/* Logo */}
-          <figure className='logo-container mb-4 mb-md-5'>
+          <Link to="/" className='logo-container mb-4 mb-md-5'>
             <img src="https://res.cloudinary.com/dnwfyqslx/image/upload/v1706900398/Site/el_refugio_logo.png" alt="Logo" />
-          </figure>
+          </Link>
 
           {/* Text */}
           <h1 className='fw-bold lh-1 mb-2 mb-md-3'>Bienvenido <br />nuevamente!</h1>
